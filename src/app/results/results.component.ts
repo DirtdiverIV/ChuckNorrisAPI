b668289390534804c58437ec1d7df390ed8f412f
DirtdiverIV <davidgomez.seg@gmail.com>
2023-07-06 21:14:46 +0200

    Sliced array results for easy view

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface ChuckNorrisJoke {
@@ -10,19 +10,15 @@ interface ChuckNorrisJoke {
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent {
   jokes: string[] = [];
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.getChuckNorrisJokes();
-  }
-
   getChuckNorrisJokes() {
-    const searchTerm = 'Chuck Norris'; // Término de búsqueda
+    const searchTerm = 'Chuck Norris';
     const url = `https://api.chucknorris.io/jokes/search?query=${encodeURIComponent(searchTerm)}`;
-  
+
     this.http.get<any>(url)
       .subscribe(response => {
         const jokes: ChuckNorrisJoke[] = response.result;
@@ -32,7 +28,17 @@ export class ResultsComponent implements OnInit {
   }
 
   showJokes() {
-    this.jokes = [...this.jokes];
+    const startIndex = 100;
+    const endIndex = 200;
+  
+    this.getChuckNorrisJokes();
+    this.jokes = [];
+  
+    setTimeout(() => {
+      if (this.jokes.length >= endIndex) {
+        this.jokes = this.jokes.slice(startIndex, endIndex);
+      }
+    }, 1000);
   }
 }
 
